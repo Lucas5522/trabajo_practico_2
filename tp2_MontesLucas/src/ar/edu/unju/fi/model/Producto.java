eff0e792f3776fe6813c193dda9f819bd420d04c
Lucas5522 <luceitormaser@gmail.com>
2024-04-22 19:02:43 -0300

    Creacion de los atributos, categorias, metodos

--- tp2_MontesLucas/src/ar/edu/unju/fi/model/Producto.java
+++ tp2_MontesLucas/src/ar/edu/unju/fi/model/Producto.java
@@ -1,5 +1,90 @@
 package ar.edu.unju.fi.model;
 
 public class Producto {
+	private int codigo;
+	private	String descripcion;
+	private double precioUnitario;
+	private origenFabricacion pais;
+	private  categoria objeto;
+	
+	
+	public enum origenFabricacion{
+		ARGENTINA,CHINA,BRASIL,URUGUAY
+		
+		
+	}
+	
+	public enum categoria{
+		TELEFONO,INFORMACION,ELECTROHOGAR,HERRAMIENTAS
+		
+	}
+	
+	public Producto() {
+		this.pais = origenFabricacion.ARGENTINA;
+		this.objeto = categoria.TELEFONO;
+	
+	}
 
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public origenFabricacion getPais() {
+		return pais;
+	}
+
+	public void setPais(origenFabricacion pais) {
+		this.pais = pais;
+	}
+
+	public categoria getObjeto() {
+		return objeto;
+	}
+
+	public void setObjeto(categoria objeto) {
+		this.objeto = objeto;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [codigo=" + codigo + ", descripcion=" + descripcion + ", precioUnitario=" + precioUnitario
+				+ ", pais=" + pais + ", objeto=" + objeto + "]";
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
